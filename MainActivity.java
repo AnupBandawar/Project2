2f98effea52e7fb65848af6258f596f557e35d7c
Anup Bandawar <anupbandawar2015@gmail.com>
2019-07-11 22:05:46 +0500

    first commit

---
+++ MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.android.clickcounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int count = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.textView2);
+        quantityTextView.setText(""+number);
+
+    }
+
+    private void submitCount(View view){
+        count = count +1 ;
+        display(count);
+    }
+}
